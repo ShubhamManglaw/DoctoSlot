47993bf29e5d7345e723616997a3f71890e32ed1
Nehanowal <ns7704858@gmail.com>
2026-05-05 22:54:25 +0530

    i have added css to about,services,contact and added the ui of that pages

---
+++ doctoslot/src/components/ServiceCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function ServiceCard({ icon, title, description }) {
+  return (
+    <article className="services-card">
+      <div className="services-card-icon">{icon}</div>
+      <h3 className="services-card-title">{title}</h3>
+      <p className="services-card-description">{description}</p>
+      <Link to="/book" className="services-card-button">
+        Book Now
+      </Link>
+    </article>
+  );
+}
+
+export default ServiceCard;
